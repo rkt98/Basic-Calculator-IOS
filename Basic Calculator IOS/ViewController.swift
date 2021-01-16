1211f5f459ff40042ea4337952a3fb9535e0c94b
Ryan Knut Tansey <ryanktansey@gmail.com>
2021-01-16 14:21:22 +1300

    Initial Commit

---
+++ Basic Calculator IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Calculator IOS
+//
+//  Created by Ryan Knut Tansey on 16/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Basic Calculator IOSTests/Basic_Calculator_IOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Basic_Calculator_IOSTests.swift
+//  Basic Calculator IOSTests
+//
+//  Created by Ryan Knut Tansey on 16/01/21.
+//
+
+import XCTest
+@testable import Basic_Calculator_IOS
+
+class Basic_Calculator_IOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
