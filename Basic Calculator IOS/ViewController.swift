2c3866290a7defc6e9dbb8c385d9a3deb230aed1
Ryan Knut Tansey <ryanktansey@gmail.com>
2021-01-16 15:05:32 +1300

    Linked buttons in code

--- Basic Calculator IOS/ViewController.swift
+++ Basic Calculator IOS/ViewController.swift
@@ -9,11 +9,115 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBAction func clearButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func leftPButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func rightPButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func plusButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func divideButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func minusButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func calculateButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func decimalButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func randomiserButton(_ sender: Any)
+    {
+    
+    }
+    
+    //
+    
+    @IBAction func zeroButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func oneButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func twoButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func threeButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func fourButton(_ sender: Any)
+    {
+    
+    }
+    
+    
+    @IBAction func fiveButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func sixButton(_ sender: Any)
+    {
+    
+    }
+    
+    @IBAction func sevenButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func eightButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func nineButton(_ sender: Any)
+    {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
